693ed268483f97fdf97f855b7a67b1052fd2d2ad
gustavo <gustavo.pereira@sou.inteli.edu.br>
2024-11-25 11:46:49 -0300

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,30 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { Button, Stack, Typography, Box } from "@mui/material";
+import {useRouter} from "next/navigation"
 
-export default function Home() {
+export default function LandPage() {
+  const router = useRouter()
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <Box 
+      sx={{
+        display: "flex", 
+        flexDirection: "column", 
+        justifyContent: "center", 
+        alignItems: "center", 
+        minHeight: "100vh",
+        textAlign: "center"
+      }}
+    >
+      <Typography variant="h1" gutterBottom>
+        Oi Murilo, Bem vindo!
+      </Typography>
+      <Typography variant="h2" gutterBottom>
+        Selecione a experiência imersiva que você deseja:
+      </Typography>
+      <Stack spacing={2} direction="row" sx={{ mt: 4 }}>
+        <Button variant="contained" sx={{height: '100px', width: '300px', fontSize:'1.2rem'}} onClick={() => router.push("/ar-experience.html")}>Realidade Aumentada</Button>
+        <Button variant="contained" sx={{height: '100px', width: '300px', fontSize:'1.2rem'}} onClick={() => router.push("/vr-experience-benchs.html")}>Realidade Virtual</Button>
+      </Stack>
+    </Box>
   );
 }
